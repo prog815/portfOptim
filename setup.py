aaeda29fb3ea5e5b1160771202fde616d4dc6a2a
prog815 <eavprog@gmail.com>
2020-08-24 21:18:45 +0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+from os.path import join, dirname
+
+import abscur
+
+setup(
+    name='portfOptim',
+    version='0.0.1',
+    description='Библиотека оптимизации финансовых портфелей',
+    packages=find_packages(),
+    long_description=open(join(dirname(__file__), 'README.md')).read(),
+    include_package_data=True,
+    author_email='eavprog@gmail.com'
+)
