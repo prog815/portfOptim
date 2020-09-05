83812dc90bfd83ae7c897349cf48b395f9c1bdd4
prog815 <eavprog@gmail.com>
2020-09-05 11:43:38 +0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup, find_packages
 from os.path import join, dirname
 
-import lib
+import portfOptim
 
 setup(
     name='portfOptim',
-    version='0.0.1',
+    version='0.0.2',
     description='Библиотека оптимизации финансовых портфелей',
     packages=find_packages(),
     long_description=open(join(dirname(__file__), 'README.md')).read(),
